c88b47c452c3e907badc9980647281e1638747bf
abir_zishan <rahman2007026@stud.kuet.ac.bd>
2024-12-18 23:45:48 +0600

    Initial Commit

---
+++ GadgetShopApp_SwiftiOS/GadgetShopApp_SwiftiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GadgetShopApp_SwiftiOSApp.swift
+//  GadgetShopApp_SwiftiOS
+//
+//  Created by Abir Rahman on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GadgetShopApp_SwiftiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
