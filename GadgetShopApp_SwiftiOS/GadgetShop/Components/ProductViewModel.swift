218d9c6e64fdcb296bd33afcf6748bb0f3de66f7
Md Mushfiqur Rahman <mushfiq2000cpt@gmail.com>
2025-01-14 20:12:34 +0600

    Create Product View Model Component

---
+++ GadgetShopApp_SwiftiOS/GadgetShop/Components/ProductViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductViewModel.swift
+//  GadgetShop
+//
+//  Created by Md Mushfiqur Rahman (2007027)
+//
+
+import SwiftUI
+
+struct ProductViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductViewModel()
+}
