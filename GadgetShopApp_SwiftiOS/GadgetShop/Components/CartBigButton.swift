bfd18626f53a2dbf0ecc07a1d07f53eac4b19449
Md Mushfiqur Rahman <mushfiq2000cpt@gmail.com>
2025-01-14 19:58:44 +0600

    Create Cart Big Button

---
+++ GadgetShopApp_SwiftiOS/GadgetShop/Components/CartBigButton.swift
@@ -0,0 +1,18 @@
+//
+//  CartBigButton.swift
+//  GadgetShop
+//
+//  Created by Md Mushfiqur Rahman (2007027)
+//
+
+import SwiftUI
+
+struct CartBigButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartBigButton()
+}
